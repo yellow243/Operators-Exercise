c46a19a7dffbc8c9bfcb7fed01ddc28516580c9b
Zach Padovano <zacharyaz10@gmail.com>
2023-04-11 11:47:09 -0700

    First commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace OperatorExercise
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num1 = 7;
+            int num2 = 16;
+           
+             Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 % num2);
+          
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+            
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            
+            var answer = AreaOfCircle(radius);
+            Console.WriteLine(answer);
+
+        }
+        public static double AreaOfCircle(double radius)
+        {
+           return Math.PI* radius *radius;
         }
     }
+
+    
+
+
 }
